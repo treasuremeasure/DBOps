0d7321154926abeb3ebce2337720b0c1e236791e
Riza <orduhaniriza@gmail.com>
2025-04-11 11:36:22 +0300

    changing schema 2

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -8,9 +8,7 @@ ALTER TABLE orders
 ADD COLUMN IF NOT EXISTS date_created date DEFAULT current_date;
 
 ALTER TABLE order_product
-ADD CONSTRAINT fk_order
 FOREIGN KEY (order_id) REFERENCES orders(id);
 
 ALTER TABLE order_product
-ADD CONSTRAINT fk_product
 FOREIGN KEY (product_id) REFERENCES product(id);
